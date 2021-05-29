342d64b541ec439879920ad3474065fa09bc4de0
pheno <pheno_the_best@yahoo.com.tw>
2021-05-29 13:25:58 +0800

    add swr

--- components/HomepageHeading.js
+++ components/HomepageHeading.js
@@ -4,16 +4,8 @@ import React, { Component } from 'react';
 import {
   Button,
   Container,
-  Divider,
-  Grid,
   Header,
   Icon,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Sidebar,
-  Visibility,
 } from 'semantic-ui-react';
 
 /* Heads up!
