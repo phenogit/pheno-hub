54255325a201aed8f65935c1ad55fd018f874557
pheno <pheno_the_best@yahoo.com.tw>
2021-06-09 11:35:43 +0800

    add kandan page

---
+++ pages/kandan.js
@@ -0,0 +1,10 @@
+import { Layout } from '../components/layouts/Layout';
+import { KandanpageView } from '../components/views/KandanpageView';
+
+export default function Home() {
+  return (
+    <Layout title="Kandan">
+      <KandanpageView />
+    </Layout>
+  );
+}
