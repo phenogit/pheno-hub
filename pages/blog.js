fb5c457450ebb7e2f6ee4041ddba22a682fa5115
pheno <pheno_the_best@yahoo.com.tw>
2021-06-14 22:40:35 +0800

    show blog list

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,41 @@
+import styles from '../styles/Blog.module.css';
+import { promises as fsPromises } from 'fs';
+
 import { Layout } from '../components/layouts/Layout';
 import { BlogpageView } from '../components/views/BlogpageView';
 
-export default function Blog() {
+export const getStaticProps = async () => {
+  const markdownFiles = await fsPromises.readdir('data/blog');
+  const postList = markdownFiles.map(filename => {
+    const slug = filename.replace(/.md$/, '');
+    const [year, month, date, ...rest] = slug.split('-');
+    const updatedAt = (new Date(`${year} ${month} ${date}`)).getTime();
+    const title = rest.join(' ');
+
+    return {
+      slug,
+      updatedAt,
+      title
+    };
+  });
+
+  return {
+    props: {
+      postList
+    }
+  };
+}
+
+export default function Blog({ postList }) {
   return (
     <Layout title="Blog">
+      {
+        postList.map(post => (
+          <div className={styles.single} key={`${post.updatedAt}-${post.slug}`}>
+            {`${post.updatedAt}: ${post.title}`}
+          </div>
+        ))
+      }
       <BlogpageView />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-import styles from '../styles/Home.module.css';
-
 import { Layout } from '../components/layouts/Layout';
 import { HomepageView } from '../components/views/HomepageView';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch("https://zenquotes.io/api/random");
+  const data = await res.json();
+  return {
+    props: {
+      quote: data[0]
+    }
+  };
+}
+
+export default function Home({ quote }) {
   return (
     <div>
       <Layout title="Home">
