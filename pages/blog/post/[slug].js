27e0f53c82300e3819657406667f479adcd0ad88
pheno <pheno_the_best@yahoo.com.tw>
2021-06-21 13:35:49 +0800

    add markdown support

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,5 +1,45 @@
+import { promises as fsPromises } from "fs";
+import Markdown from "markdown-to-jsx";
+
 import { Layout } from "../../../components/layouts/Layout";
 
-export default function Post() {
-  return <Layout title="Post">You got to a blog post</Layout>;
+export async function getStaticProps({ params }) {
+  const [year, month, day, ...rest] = params.slug.split("-");
+  const updatedAt = new Date(`${year} ${month} ${day}`).getTime();
+  const title = rest.join(" ");
+
+  const content = await fsPromises.readFile(
+    `data/blog/${params.slug}.md`,
+    "utf8"
+  );
+
+  return {
+    props: {
+      post: {
+        slug: params.slug,
+        title,
+        content,
+        updatedAt,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { slug: "2021-01-26-Kit-Korner-Notes" },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export default function Post({ post }) {
+  return (
+    <Layout title="Post">
+      <Markdown>{post.content}</Markdown>
+    </Layout>
+  );
 }
