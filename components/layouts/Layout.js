551aee5d8b4bfbbf9cf3bceb895ed388612753a5
pheno <pheno_the_best@yahoo.com.tw>
2021-07-07 19:43:20 +0800

    add cursor to logo

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,11 +1,8 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import Image from 'next/image';
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 
-export function Layout({
-  children,
-  title = 'This is the default title',
-}) {
+export function Layout({ children, title = "This is the default title" }) {
   return (
     <div className="content">
       <Head>
@@ -15,13 +12,22 @@ export function Layout({
       </Head>
       <header>
         <nav>
-          <div className="logo">
-            <Image src="/logo.png" width={77} height={77} />
-            Pheno Hub
-          </div>
-          <Link href="/"><a>Home</a></Link>
-          <Link href="/kandan"><a>Kandan</a></Link>
-          <Link href="/blog"><a>Blog</a></Link>
+          <Link href="/">
+            <div className="logo cursor">
+              <Image src="/logo.png" width={33} height={33} />
+              Pheno Hub
+            </div>
+          </Link>
+
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/kandan">
+            <a>Kandan</a>
+          </Link>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
         </nav>
       </header>
 
@@ -31,5 +37,5 @@ export function Layout({
         <Image src="/email.png" width={314} height={23} />
       </footer>
     </div>
-  )
+  );
 }
